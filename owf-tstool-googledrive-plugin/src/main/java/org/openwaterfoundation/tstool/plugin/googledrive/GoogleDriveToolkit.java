5947c1ee64617905ff5b3a5429e298292c56d8ca
Steve Malers <steve.malers@openwaterfoundation.org>
2023-12-28 13:41:42 -0700

    Add prefix to paths to help understand Google Drive location, issue #6

--- owf-tstool-googledrive-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/googledrive/GoogleDriveToolkit.java
+++ owf-tstool-googledrive-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/googledrive/GoogleDriveToolkit.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.DriveList;
 import com.google.api.services.drive.model.FileList;
 
 import RTi.Util.Message.Message;
@@ -35,6 +36,8 @@ import RTi.Util.Message.Message;
 /**
  * Google Drive toolkit singleton.
  * Retrieve the instance with getInstance() and then use methods.
+ * This toolkit contains methods for useful tasks,
+ * such as looking up a Google Drive file/folder identifier given its path.
  */
 public class GoogleDriveToolkit {
 	/**
@@ -67,7 +70,7 @@ public class GoogleDriveToolkit {
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Private constructor.
 	 */
@@ -228,8 +231,6 @@ public class GoogleDriveToolkit {
         	// - only match the folder name
         	// The following works with the OAuth authentication.
             String q = "name='" + folderName + "' and '" + currentFolderId + "' in parents";
-        	// Try getting the files in the root folder.
-            //String q = "'" + currentFolderId + "' in parents";
             Message.printStatus(2, routine, "Getting files using q=\"" + q + "\"");
             // Only the file ID is needed.
             FileList result = driveService.files().list()
@@ -260,6 +261,131 @@ public class GoogleDriveToolkit {
     }
 
 	/**
+	 * Get the Google Drive shared drives folder ID given a path to the folder.
+	 * This should be called for folders that are in shared drives (but not shared folders).
+	 * This ensures that the path is not mixed up with normal My Drive,
+	 * such as service accounts where typically only shared resources are available.
+	 * @param driveService Drive service object
+	 * @param sharedDrivePath shared folder path using syntax "/Shared drives/driveName/path/to/folder/"
+	 * The leading and trailing / are optional.
+	 * @return the Google Drive folder ID, or null if not matched
+	 * @throws IOException
+	 */
+	public String getFolderIdForSharedDrivesPath ( Drive driveService, String sharedDrivePath ) throws IOException {
+		String routine = getClass().getSimpleName() + ".getFolderIdForSharedDrivesPath";
+
+		// Remove the leading "Shared drives" and "/Shared drives".
+		if ( sharedDrivePath.equals(SHARED_DRIVES) ) {
+			// Did not match a specific sub-folder.
+			return null;
+		}
+		else if ( sharedDrivePath.startsWith("/" + SHARED_DRIVES) ) {
+			// Remove the leading "/Shared drives".
+			sharedDrivePath = sharedDrivePath.substring(SHARED_DRIVES.length() + 1);
+		}
+		else if ( sharedDrivePath.startsWith(SHARED_DRIVES) ) {
+			// Remove the leading "Shared drives".
+			sharedDrivePath = sharedDrivePath.substring(SHARED_DRIVES.length());
+		}
+		else {
+			// No leading "Shared drives" variation.
+		}
+
+		if ( sharedDrivePath.startsWith("/") ) {
+			// Remove the leading slash because it will result in an empty path below.
+			if ( sharedDrivePath.length() == 1 ) {
+				return null;
+			}
+			else {
+				sharedDrivePath = sharedDrivePath.substring(1);
+			}
+		}
+		if ( sharedDrivePath.endsWith("/") ) {
+			// Remove the trailing slash because it will result in an empty path below.
+			if ( sharedDrivePath.length() == 1 ) {
+				return null;
+			}
+			else {
+				sharedDrivePath = sharedDrivePath.substring(0,sharedDrivePath.length());
+			}
+		}
+
+		// Split the path into parts that can be checked against Google Drive folders:
+		// - the first part is the drive name
+        String[] folderNames = sharedDrivePath.split("/");
+
+        // For shared folders can't use "root" because that is used with My Drive
+        // sharing user, not the current user.
+        // Therefore list shared folders and match the name with the first part of the path.
+
+        // List the shared drives to match the first part and then use that for the initial folder ID.
+
+   		// Output the names and IDs for up to 10 files.
+   		DriveList result0 = null;
+   		Drive.Drives.List request = null;
+		// Do the initial request.
+   		String q0 = "name='" + folderNames[0] + "'";
+		request = driveService
+			// Request to execute.
+			.drives()
+			// Holds the parameters for the request.
+			.list()
+			// Query the shared drive name.
+			.setQ(q0)
+			// Set the fields that are returned (creation time, etc.).
+   			.setFields("*");
+		// Invoke the remote operation.
+		result0 = request.execute();
+		
+		if ( (result0 == null) || (result0.size() == 0) ) {
+			// No shared drives so return null;
+			Message.printStatus(2, routine, "Could not match shared drive \"" + folderNames[1] + "\".");
+			return null;
+		}
+
+		// Get the drive ID for the matching shared drive.
+		String currentFolderId = result0.getDrives().get(0).getId();
+
+        // Iterate through each folder in the path:
+   		// - skip the first part, which was checked above
+        for ( int i = 1; i < folderNames.length; i++ ) {
+        	String folderName = folderNames[i];
+            // Search for the folder by name in the parent folder:
+        	// - first time through will list top shared drive, then sub-folders
+        	// - only match the folder name
+            String q = "mimeType='application/vnd.google-apps.folder' and name='" + folderName + "' and '"
+            	+ currentFolderId + "' in parents"; // and '"; + currentFolderId + "' in drive";
+            Message.printStatus(2, routine, "Getting files using q=\"" + q + "\"");
+            // Only the file ID is needed.
+            FileList result = driveService.files().list()
+                .setQ(q)
+                .setFields("files(id)")
+                .execute();
+
+            // Check if the folder (path part) was found.
+            if ( (result.getFiles() != null) && !result.getFiles().isEmpty()) {
+                // Update the current folder ID for the next iteration.
+                currentFolderId = result.getFiles().get(0).getId();
+                Message.printStatus(2, routine, "Set currentFolderId=\"" + currentFolderId + "\"");
+            }
+            else {
+                // Folder not found, return null.
+            	if ( result.getFiles() == null ) {
+            		Message.printStatus(2, routine, "getFiles() returned null. Returning null.");
+            	}
+            	else if ( result.getFiles().isEmpty() ) {
+            		Message.printStatus(2, routine, "getFiles() returned empty list. Returning null.");
+            	}
+                return null;
+            }
+        }
+
+        // Return the final folder ID.
+   		Message.printStatus(2, routine, "Found ID " + currentFolderId + " for shared folder path \"" + sharedDrivePath + "\".");
+        return currentFolderId;
+	}
+
+	/**
 	 * Get the Google Drive shared folder ID given a path to the folder.
 	 * This should be called for folders that are shared (but not shared drives).
 	 * This ensures that the path is not mixed up with normal My Drive,
@@ -272,7 +398,7 @@ public class GoogleDriveToolkit {
 	 */
 	public String getFolderIdForSharedWithMePath ( Drive driveService, String sharedFolderPath ) throws IOException {
 		String routine = getClass().getSimpleName() + ".getFolderIdForSharedWithMePath";
-		
+
 		// Remove the leading "Shared with me" and "/Shared with me".
 		if ( sharedFolderPath.equals(SHARED_WITH_ME) ) {
 			// Did not match a specific sub-folder.
@@ -289,7 +415,7 @@ public class GoogleDriveToolkit {
 		else {
 			// No leading "Shared with me" variation.
 		}
-		
+
 		if ( sharedFolderPath.startsWith("/") ) {
 			// Remove the leading slash because it will result in an empty path below.
 			if ( sharedFolderPath.length() == 1 ) {
@@ -311,14 +437,14 @@ public class GoogleDriveToolkit {
 
 		// Split the path into parts that can be checked against Google Drive folders.
         String[] folderNames = sharedFolderPath.split("/");
-        
+
         // For shared folders can't use "root" because the shared folder exists in the root of the
         // sharing user, not the current user.
-        // Therefore ilst shared folders and match the name with the first part of the path.
+        // Therefore list shared folders and match the name with the first part of the path.
 
         // List the top-level folders to match the first part and then use that for the initial folder ID:
         // - sharedWithMe=true is required to list the shared folder (but is not required below to list subfolder contents)
-        
+
         String q0 = "mimeType='application/vnd.google-apps.folder' and sharedWithMe=true";
         Message.printStatus(2, routine, "Getting files using q=\"" + q0 + "\"");
         // Only the file ID is needed.
@@ -326,7 +452,7 @@ public class GoogleDriveToolkit {
             .setQ(q0)
             .setFields("files(id)")
             .execute();
-        
+
    		List<com.google.api.services.drive.model.File> files = result0.getFiles();
         String currentFolderId = null;
    		if ( (files == null) || files.isEmpty() ) {
@@ -346,8 +472,6 @@ public class GoogleDriveToolkit {
         	// - only match the folder name
         	// - using sharedWithMe=true here will return zero entries because only the top-level is shared?
             String q = "name='" + folderName + "' and '" + currentFolderId + "' in parents";
-        	// Try getting the files in the root folder.
-            //String q = "'" + currentFolderId + "' in parents";
             Message.printStatus(2, routine, "Getting files using q=\"" + q + "\"");
             // Only the file ID is needed.
             FileList result = driveService.files().list()
@@ -377,7 +501,7 @@ public class GoogleDriveToolkit {
    		Message.printStatus(2, routine, "Found ID " + currentFolderId + " for shared folder path \"" + sharedFolderPath + "\".");
         return currentFolderId;
 	}
-	
+
 	/**
 	 * Get the parent path given the parent folder Google Drive ID.
 	 * @param driveService Drive service instance
@@ -454,7 +578,21 @@ public class GoogleDriveToolkit {
         }
         return parentFoldersSorted;
     }
-    
+
+	/**
+	 * Determine whether the path starts with '/Shared drives' or a variation.
+	 * @return true if a "Shared drives" path
+	 */
+	public boolean pathStartsWithSharedDrives ( String folderPath ) {
+		String slashSharedDrives = "/" + SHARED_DRIVES;
+		if ( folderPath.startsWith(SHARED_DRIVES) || folderPath.startsWith(slashSharedDrives) ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
 	/**
 	 * Determine whether the path starts with '/Shared with me' or a variation.
 	 * @return true if a "Shared with me" path
